78d4a893a3daeee0f5851c7ba4852cef6f725ef9
Etsubie06 <etsubtaye4@gmail.com>
2024-07-13 15:28:22 +0300

    restructure folders

--- lib/core/colors/colors.dart
+++
@@ -1,8 +0,0 @@
-// i want to save this color #3563E9 here and use in the User interface
-import 'package:flutter/material.dart';
-
-class ThemeColors {
-  static const Color primary = Color(0xff5C95CA);
-  static const Color yellowSecondary = Color(0xffFFD899);
-  static const Color buttonColor = Color(0xff146D95);
-}
