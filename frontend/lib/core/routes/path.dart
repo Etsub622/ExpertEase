24c5c9a00aca7ff06222793e33845ceccaaaf858
Lelina622 <115922945+Etsub622@users.noreply.github.com>
2024-07-16 14:50:22 +0300

    Update path.dart

--- frontend/lib/core/routes/path.dart
+++ frontend/lib/core/routes/path.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class AppPath {
   static const home = '/';
   static const login = '/login';
@@ -12,6 +14,13 @@ class AppPath {
   static const onboard3 = '/onboard3';
   static const onboard4 = '/onboard4';
   static const expertDisplay = '/expertDisplay';
+  static const feedPage = '/feed_page';
+  static const expertProfile = '/expert_profile';
+  static const expertDetail = '/expert_detail';
+  static const clientProfile = '/client_profile';
+  static const schedule = '/schedule_page';
+  static const review = '/review';
+  static const notification = '/notification';
   static const splash = '/splash';
   static const welcome = '/welcome';
   static const expertSignUp = '/expertSignUp';
