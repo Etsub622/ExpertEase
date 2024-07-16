2f0f675cfb00cb6a8841cf903238cd30d3ca46db
dagmaros27 <dagxx27@gmail.com>
2024-07-16 13:24:13 +0300

    implemented endpoints (not tested)

--- backend/ExpertEase/index.js
+++ backend/ExpertEase/index.js
@@ -2,12 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const db = require("./db");
 const chatRoutes = require("./routes/chatbotRoutes");
+const clientRoutes = require("./routes/clientRoutes");
+const expertRoutes = require("./routes/expertRoutes");
+const appointmentRoutes = require("./routes/appointmentRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use("/", chatRoutes);
+app.use("/clients", clientRoutes);
+app.use("/experts", expertRoutes);
+app.use("/appointments", appointmentRoutes);
+app.use("/reviews", reviewRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
