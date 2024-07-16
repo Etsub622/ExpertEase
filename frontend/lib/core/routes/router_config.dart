fe2aa26fbcf05cf4bddbdf2d679d864c611b072d
Lelina622 <115922945+Etsub622@users.noreply.github.com>
2024-07-16 14:47:46 +0300

    Update router_config.dart

--- frontend/lib/core/routes/router_config.dart
+++ frontend/lib/core/routes/router_config.dart
@@ -1,116 +1,134 @@
 import 'package:expertease/core/routes/path.dart';
 import 'package:expertease/features/Feed/presentation/pages/feed_page.dart';
+import 'package:expertease/features/Splash_screens/presentation/pages/expert_sign_up.dart';
+import 'package:expertease/features/Splash_screens/presentation/pages/expertee_sign_up.dart';
+import 'package:expertease/features/Splash_screens/presentation/pages/login.dart';
 import 'package:expertease/features/Splash_screens/presentation/pages/onboard1.dart';
 import 'package:expertease/features/Splash_screens/presentation/pages/onboard2.dart';
 import 'package:expertease/features/Splash_screens/presentation/pages/onboard3.dart';
 import 'package:expertease/features/Splash_screens/presentation/pages/onboard4.dart';
+import 'package:expertease/features/Splash_screens/presentation/pages/splash_screen.dart';
+import 'package:expertease/features/Splash_screens/presentation/pages/welcome.dart';
+import 'package:expertease/features/appointment_display/presentation/pages/expert_profile_page.dart';
+import 'package:expertease/features/client_display/presentation/pages/client_profile.dart';
+import 'package:expertease/features/client_display/presentation/pages/expert_detail_page.dart';
 import 'package:expertease/features/client_display/presentation/pages/notification.dart';
 import 'package:expertease/features/client_display/presentation/pages/shecdule_page.dart';
 import 'package:expertease/features/expert_display/presentation/pages/experts.dart';
 import 'package:expertease/features/video_confrence/widget/rate_session.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:expertease/features/appointment_display/presentation/pages/expert_profile_page.dart';
-import 'package:expertease/features/client_display/presentation/pages/expert_detail_page.dart';
-import 'package:expertease/features/client_display/presentation/pages/client_profile.dart';
-
 
 class AppRouter {
-  static final GoRouter router = GoRouter(
-    initialLocation: AppPath.notification,
-    routes: <GoRoute>[
-      GoRoute(
+  static final GoRouter router =
+      GoRouter(initialLocation: AppPath.splash, routes: <GoRoute>[
+    GoRoute(
         path: AppPath.home,
         name: AppPath.home,
         builder: (BuildContext context, GoRouterState state) {
           return FeedPage();
-        },
-      ),
-      GoRoute(
+        }),
+    GoRoute(
         path: AppPath.onboard1,
         name: AppPath.onboard1,
         builder: (BuildContext context, GoRouterState state) {
           return OnboardOne();
-        },
-      ),
-      GoRoute(
+        }),
+    GoRoute(
         path: AppPath.onboard2,
         name: AppPath.onboard2,
         builder: (BuildContext context, GoRouterState state) {
           return OnBoardTwo();
-        },
-      ),
-      GoRoute(
+        }),
+    GoRoute(
         path: AppPath.onboard3,
         name: AppPath.onboard3,
         builder: (BuildContext context, GoRouterState state) {
           return OnBoardThree();
-        },
-      ),
-      GoRoute(
+        }),
+    GoRoute(
         path: AppPath.onboard4,
         name: AppPath.onboard4,
         builder: (BuildContext context, GoRouterState state) {
           return onBoardFour();
-        },
-      ),
-      GoRoute(
+        }),
+    GoRoute(
         path: AppPath.expertDisplay,
         name: AppPath.expertDisplay,
         builder: (BuildContext context, GoRouterState state) {
           return ExpertDisplay();
-        },
-      ),
-      GoRoute(
-        path: AppPath.feedPage,
-        name: AppPath.feedPage,
+        }),
+    GoRoute(
+        path: AppPath.splash,
+        name: AppPath.splash,
         builder: (BuildContext context, GoRouterState state) {
-          return FeedPage();
-        },
-      ),
-      GoRoute(
-        path: AppPath.expertProfile,
-        name: AppPath.expertProfile,
+          return SplashScreen();
+        }),
+    GoRoute(
+        path: AppPath.login,
+        name: AppPath.login,
         builder: (BuildContext context, GoRouterState state) {
-          return ExpertProfilePage();
-        },
-      ),
-      GoRoute(
-        path: AppPath.expertDetail,
-        name: AppPath.expertDetail,
+          return LogIn();
+        }),
+    GoRoute(
+        path: AppPath.welcome,
+        name: AppPath.welcome,
         builder: (BuildContext context, GoRouterState state) {
-          return ExpertDetailPage(expert: state.extra as Map<String, dynamic>);
-        },
-      ),
-      GoRoute(
-        path: AppPath.clientProfile,
-        name: AppPath.clientProfile,
+          return WelcomeScreen();
+        }),
+    GoRoute(
+        path: AppPath.expertSignUp,
+        name: AppPath.expertSignUp,
         builder: (BuildContext context, GoRouterState state) {
-          return ClientProfilePage();
-        },
-      ),
-      GoRoute(
-        path: AppPath.schedule,
-        name: AppPath.schedule,
+          return ExpertSignUP();
+        }),
+    GoRoute(
+        path: AppPath.expertee,
+        name: AppPath.expertee,
         builder: (BuildContext context, GoRouterState state) {
-          return SchedulePage();
-        },
-      ),
-
-      GoRoute(
-        path: AppPath.review,
-        name: AppPath.review,
-        builder: (BuildContext context, GoRouterState state) {
-          return RateSessionPage();
-        },
-      ),
-      GoRoute(
-        path: AppPath.notification,
-        name: AppPath.notification,
-        builder: (BuildContext context, GoRouterState state) {
-          return NotificationPage();
-        },
-      ),
-    ],
-  );
+          return ExperteeSignUp();
+        }),
+    GoRoute(
+      path: AppPath.expertProfile,
+      name: AppPath.expertProfile,
+      builder: (BuildContext context, GoRouterState state) {
+        return ExpertProfilePage();
+      },
+    ),
+    GoRoute(
+      path: AppPath.expertDetail,
+      name: AppPath.expertDetail,
+      builder: (BuildContext context, GoRouterState state) {
+        return ExpertDetailPage(expert: state.extra as Map<String, dynamic>);
+      },
+    ),
+    GoRoute(
+      path: AppPath.clientProfile,
+      name: AppPath.clientProfile,
+      builder: (BuildContext context, GoRouterState state) {
+        return ClientProfilePage();
+      },
+    ),
+    GoRoute(
+      path: AppPath.schedule,
+      name: AppPath.schedule,
+      builder: (BuildContext context, GoRouterState state) {
+        return SchedulePage();
+      },
+    ),
+    GoRoute(
+      path: AppPath.review,
+      name: AppPath.review,
+      builder: (BuildContext context, GoRouterState state) {
+        return RateSessionPage();
+      },
+    ),
+    GoRoute(
+      path: AppPath.notification,
+      name: AppPath.notification,
+      builder: (BuildContext context, GoRouterState state) {
+        return NotificationPage();
+      },
+    ),
+  ]);
 }
